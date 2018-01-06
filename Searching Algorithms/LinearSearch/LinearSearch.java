5fce4a9154f4bc1c362c50bb54b24bb57856d7c6
ashu-beckham <201552065@iiitvadodara.ac.in>
2018-01-06 17:43:19 +0530

    Name changed

--- Searching Algorithms/LinearSearch/LinearSearch.java
+++ Searching Algorithms/LinearSearch/LinearSearch.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Linear_search  {
+public class LinearSearch  {
     public static void main (String[] args)  {
         int[] a = {7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 10};
         int indx = linear_search(a, 5);
