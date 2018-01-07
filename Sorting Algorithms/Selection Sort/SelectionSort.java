3cf2cb7d568b2adf2917aff6f43f7e4aaa72f552
TarunISCO <201552063@iiitvadodara.ac.in>
2018-01-07 11:31:51 +0530

    updated class and file names

--- Sorting Algorithms/Selection Sort/SelectionSort.java
+++ Sorting Algorithms/Selection Sort/SelectionSort.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Selection_sort  {
+public class SelectionSort  {
 
     public static void main (String[] args) {
         Scanner in = new Scanner(System.in);
