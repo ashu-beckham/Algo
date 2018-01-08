a2a18c19d3b66ce0ec7dd672811763afb93bc9e7
ashu-beckham <201552065@iiitvadodara.ac.in>
2018-01-08 18:24:47 +0530

    Added binary Search

---
+++ Searching Algorithms/BinarySearch/BinarySearch.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class BinarySearch  {
+    public static void main (String[] args) {
+        int[] a = {4, 5, 3, 2, 1, 6, 7, 89, 10};
+        Arrays.sort(a); //Array should be sorted.
+        int ind = binarySearch(a, 89);
+        if (ind == -1)
+            System.out.println("Element not found!!!!");
+        else
+            System.out.println("index of 89 in sorted array is : " + ind);
+
+    }
+    static int binarySearch(int[] a, int x) {
+        int l = 0, r = a.length;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (a[mid] == x)
+                return mid;
+            if (a[mid] < x)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return -1;
+    }
+}
